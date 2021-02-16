2bc91b7245c3683c6e54b84b38aef66b4fcc28f6
kyledobash <kyledobash@yahoo.com>
2021-02-16 14:24:42 -0700

    Built the employer model

---
+++ Models/Employer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLOS_Group_Capstone.Models
+{
+    public class Employer
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public int PhoneNumber { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+    }
+}
