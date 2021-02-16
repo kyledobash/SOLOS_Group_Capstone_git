3d83aeb50a88c8cb39208c8e83adda52b3ce9939
kevin petroviak <kpetro18@gmail.com>
2021-02-16 15:51:00 -0600

    added Resume model properties and Developer model properties

---
+++ Models/Developer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLOS_Group_Capstone.Models
+{
+    public class Developer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public int PhoneNumber { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Display(Name = "Bookmarked Job Listings")]
+        public double BookMarkedJobListing { get; set; } // employeer id . job id ( ex employee with id 4 and job listing with id 18 = 4.18
+        [Display(Name = "Pending Applications")]
+        public int Pending_applications { get; set; }
+
+        [ForeignKey("JobCast")] //double check that this matches with jobcast junction
+        public int JobCastId { get; set; } // get first value as key for jobcastId . then be able to access applicationdbcontext through LINQ . notation.
+    }
+}
---
+++ Models/Resume.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLOS_Group_Capstone.Models
+{
+    public class Resume
+    {
+        [key]
+        public int Id { get; set; }
+
+        [Display(Name = "Developer Specialty")]
+        public string Dev_specialty { get; set; }
+
+        [Display(Name = "Languages")]
+        public string Languages { get; set; }
+
+        [Display(Name = "Education and Certificates")]
+        public string Education_Certificates { get; set; }
+
+        [Display(Name = "Prjoects")]
+        public string Projects { get; set; }
+
+        [Display(Name = "Web Portfolio")]
+        public string Web_Portfolio { get; set; }
+
+        [Display(Name = "Resume Copy")]
+        public string Resume_Copy { get; set; }
+    }
+}
