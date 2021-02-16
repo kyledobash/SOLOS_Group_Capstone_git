fb06343191f0e44c9269e6340f2fbdec1961f54d
kyledobash <kyledobash@yahoo.com>
2021-02-16 14:32:57 -0700

    Created the Jobs model

---
+++ Models/Jobs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLOS_Group_Capstone.Models
+{
+    public class Jobs
+    {
+        [Key]
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public string Requirements { get; set; }
+        public string Descriptions { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+
+        [ForeignKey("EmployerId")]
+        public int EmpId { get; set; }
+        public Employer EmployerId { get; set; }
+    }
+}
