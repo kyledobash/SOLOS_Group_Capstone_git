d55b7d45670d130d33dd81ac5ef25f64886ecc60
unknown <chue.vang0@gmail.com>
2021-02-19 15:38:41 -0600

    File upload UI is fine, functionality is needs more work.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SOLOS_Group_Capstone.Models;
 using System;
@@ -32,6 +33,35 @@ namespace SOLOS_Group_Capstone.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            // Extract file name from whatever was posted by browser
+            var fileName = System.IO.Path.GetFileName(file.FileName);
+
+            // If file with same name exists delete it
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.File.Delete(fileName);
+            }
+
+            // Create new local file and copy contents of uploaded file
+            using (var localFile = System.IO.File.OpenWrite(fileName))
+            using (var uploadedFile = file.OpenReadStream())
+            {
+                uploadedFile.CopyTo(localFile);
+            }
+
+            ViewBag.Message = "File successfully uploaded";
+
+            return View();
         }
     }
 }
